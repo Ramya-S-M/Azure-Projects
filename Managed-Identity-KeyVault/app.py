6a47e0afab1eab3cb2556862ef75405e8c05c090
Ramya S <ramyasubbu005@gmail.com>
2026-04-15 19:57:12 +0530

    Added code files

---
+++ Managed-Identity-KeyVault/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    secret = os.getenv("DB_PASSWORD", "No secret found")
+    return f"""
+    <h1>Managed Identity + Key Vault</h1>
+    <p><b>Secret from Key Vault:</b> {secret}</p>
+    """
+
+if __name__ == "__main__":
+    app.run()
