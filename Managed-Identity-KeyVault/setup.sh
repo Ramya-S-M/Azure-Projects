5ac4425b34e08691299bd85ffcc478342941ffb9
Ramya S <ramyasubbu005@gmail.com>
2026-04-16 14:46:34 +0530

    ManagedKeyvaultsecret

--- Managed-Identity-KeyVault/setup.sh
+++ Managed-Identity-KeyVault/setup.sh
@@ -1,37 +1,29 @@
 az login --use-device-code
 
 
-RG="RG-Secure-Identity"
-LOC="centralindia"
-RAND=$RANDOM
-KV="kv-secure-vault-$RAND"
-APP="webapp-secure-$RAND"
-PLAN="ServicePlan-Linux"
+RESOURCE_GROUP="rg-myapp-cd"
+LOCATION="eastus"
+APP_NAME="myapp-ramya"
+KEY_VAULT_NAME="kvapp-web-ramya"
+APP_PLAN="plan-myapp"
 
+echo $APP_NAME
+echo $KEY_VAULT_NAME
 
 echo "Building infrastructure..."
-az group create --name $RG --location $LOC
-az keyvault create --name $KV --resource-group $RG --enable-rbac-authorization true
-az appservice plan create --name $PLAN --resource-group $RG --sku S1 --is-linux
-az webapp create --name $APP --resource-group $RG --plan $PLAN --runtime "PYTHON:3.11"
-
-
+az group create --name $RESOURCE_GROUP --location $LOCATION
+az keyvault create --name $KEY_VAULT_NAME --resource-group $RESOURCE_GROUP --enable-rbac-authorization true
+az appservice plan create --name $APP_PLAN --resource-group $RESOURCE_GROUP --sku S1 --is-linux
+az webapp create --name $APP_NAME --resource-group $RESOURCE_GROUP --plan $APP_PLAN --runtime "PYTHON:3.11"
 
 echo "Building Managed Identity..."
-MSI_ID=$(az webapp identity assign --name $APP --resource-group $RG --query principalId -o tsv)
+MSI_ID=$(az webapp identity assign --name $APP_NAME --resource-group $RESOURCE_GROUP --query principalId -o tsv)
 
 echo "Granting RBAC role..."
-KV_SCOPE=$(az keyvault show --name $KV --query id -o tsv)
+KV_SCOPE=$(az keyvault show --name $KEY_VAULT_NAME --query id -o tsv)
 az role assignment create --role "Key Vault Secrets User" --assignee-object-id $MSI_ID --assignee-principal-type ServicePrincipal --scope $KV_SCOPE
 
 echo "Securing the Secret..."
-az keyvault secret set --vault-name $KV --name "DbPassword" --value "MyGrandSecret123!"
-
-SECRET_URI=$(az keyvault secret show --name "DbPassword" --vault-name $KV --query id -o tsv)
-
-az webapp config appsettings set --name $APP --resource-group $RG --settings DB_PASSWORD="@Microsoft.KeyVault(SecretUri=$SECRET_URI)"
-
-echo "Done"
-echo "Resource Group: $RG"
-echo "Key Vault: $KV"
-echo "Web App: $APP"
+az keyvault secret set --vault-name $KEY_VAULT_NAME --name "DbPassword" --value "MyGrandSecret123!"
+SECRET_URI=$(az keyvault secret show --name "DbPassword" --vault-name $KEY_VAULT_NAME --query id -o tsv)
+az webapp config appsettings set --name $APP_NAME --resource-group $RESOURCE_GROUP --settings DB_PASSWORD="@Microsoft.KeyVault(SecretUri=$SECRET_URI)"
